73ff878ae75a91c14100d8b2ab859c1ca1422a5d
Davi Luna <davijluna@gmail.com>
2022-02-18 00:31:43 -0300

    exercicio 7

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const testo = document.querySelector('#texto-tarefa');
 const recebeLi = document.querySelector('#lista-tarefas');
 const botao = document.querySelector('#criar-tarefa');
+const cor = document.querySelector
 
 function area() {
-
-let li = document.createElement('li');
-li.innerText = testo.value
- recebeLi.appendChild(li)  
- testo.value = '';
+   let elemento = document.createElement('li');
+   elemento.innerText = testo.value
+   recebeLi.appendChild(elemento)  
+   testo.value = '';
+   elemento.addEventListener('click', 
+   
+   function clicou(){
+     elemento.className = 'selecao'; 
+   })
 }
 
 botao.addEventListener('click', area);
+
+
+function nome() {
+
+}
