5e9ce7fea6eaf245b47faeef42d38ae0fdabb90c
davi <davijluna@gmail.com>
2023-05-05 00:30:37 -0300

    feat: adicionando arquivos e limpando arquivos da trybe para disponibilizar no repositorio pessoal

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
+// constantes ids do html para facilitar a localizção.
+
 const testo = document.querySelector('#texto-tarefa');
 const recebeLi = document.querySelector('#lista-tarefas');
 const botao = document.querySelector('#criar-tarefa');
 const botaoApagarTudo = document.querySelector('#apaga-tudo');
 
+// Fucntion que cria o li e apaga texto do input /
 
 function adicionarLi() {
-   const elemento = document.createElement('li');
-   elemento.innerText = testo.value
-   recebeLi.appendChild(elemento) 
-   elemento.className = 'selecao'
-   testo.value = '';
-   
-recebeLi.addEventListener('click', function (event){
+  const elemento = document.createElement('li');.
+  elemento.innerText = testo.value;
+  recebeLi.appendChild(elemento);
+  elemento.className = 'selecao';
+  testo.value = '';
+
+  recebeLi.addEventListener('click', (event) => {
     if (event.target.id !== 'lista-tarefas') {
       for (let i = 0; i < recebeLi.children.length; i += 1) {
         if (recebeLi.children[i].style.backgroundColor === 'rgb(128, 128, 128)') {
-        recebeLi.children[i].style.backgroundColor = '';
+          recebeLi.children[i].style.backgroundColor = '';
         }
       }
       event.target.style.backgroundColor = 'rgb(128, 128, 128)'; // fundo cinza na tarefa selecionada
     }
   });
-      
-    }   
+}
 botao.addEventListener('click', adicionarLi);
-
-recebeLi.addEventListener('dblclick',function (event){
-    const eventoAlvo = event.target;
-    if(eventoAlvo.classList.contains('completed')){
-     eventoAlvo.classList.remove('completed');
-    }else{
+/**
+ * Tive explicação de Guilherme Palma na sala de estudos sobre o event target
+ */
+recebeLi.addEventListener('dblclick', (event) => {
+  const eventoAlvo = event.target;
+  if (eventoAlvo.classList.contains('completed')) {
+    eventoAlvo.classList.remove('completed');
+  } else {
     eventoAlvo.classList.add('completed');
-    }
-})
+  }
+});
